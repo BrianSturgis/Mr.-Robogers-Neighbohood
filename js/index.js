406465783e588d72f5ff68ecf94b40e282ef6518
Brian Sturgis <sturujisu@gmail.com>
2020-10-30 15:06:13 -0700

    a scheduled commit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
-const sibNames = ["josh", "jozy", "maggy", "georgia"]
-const parentsNames = ["kay", "hugh", "greg","dede"]
-const family = sibNames.concat(parentsNames);
-// console.log(family)
-let familynpets = family.concat("hobbes")
-console.log(familynpets)
+$(document).ready(function() {
+  $("form#inputNumber").submit(function(event) {
+  const input = parseInt($("#numbers").val());
+
+  // console.log(input)
+  alert(input)
+
+  }
+
+});
