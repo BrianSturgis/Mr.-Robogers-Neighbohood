398915ede4f4c874e42d39344e8a3bd29712c38d
Brian Sturgis <sturujisu@gmail.com>
2020-10-31 11:04:38 -0700

    the first commit of the day

--- js/index.js
+++ js/index.js
@@ -1,28 +1,15 @@
-// for (let num = 0; num <= 99; num += 1) {
-//   console.log(num)
-// }
-// let RANGE = []
 
 
 
 
-// // UI logic//
-// $(document).ready(function() {
-//   $("form#inputNumber").submit(function(event) {
-//     let num = parseInt($("#numbers").val());
-    // console.log(num)
+
+  $(document).ready(function() {
+  $("form#inputNumber").submit(function(event) {
+  let num = parseInt($("#numbers").val());
+  let num =  
+    console.log(num)
     
-//   event.preventDefault();
-//   });
-// });
-
-  
-
-// for (let i = 0; i < 99; i++) {
-//   console.log('in loop:', i);
-
-// }
-// // console.log('loop finished')
-
-
-const = []
+  event.preventDefault();
+  });
+});
+// 
