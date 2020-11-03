9732fa898b35ecd1894b3e9e1b88ff9f2b880ffe
Brian Sturgis <sturujisu@gmail.com>
2020-11-02 22:11:42 -0800

    will clean up js file post succsessful test of getRange function

--- js/index.js
+++ js/index.js
@@ -3,21 +3,11 @@
 $(document).ready(function(){
   $("#inputNumber").submit(function(event) {
     event.preventDefault();
-    console.log("hi")
     let userInput = $("#numbers").val();
     const originalInput = $("#numbers").val();
-    console.log(originalInput)
     const replacementWords = ["Won't you be my neighbor?", "Boop!", "Beep!"];
-    console.log(replacementWords)
     const numberCheck = ["3", "2", "1"];
-    console.log(numberCheck);
     let range = [];
-    console.log(range)
-
-
-
-
-
   function getRange(userInput) {
     range = [];
     let userNumber = parseInt(userInput);
@@ -27,8 +17,9 @@ $(document).ready(function(){
     } while( UsernNumber > 0)
     range.reverse();
     return range;
+    
     }
-      
+    console.log(getRange) 
     
   
   
