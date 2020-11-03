ec87744d755343be9c0ca75edd054c9c98d99abf
Brian Sturgis <sturujisu@gmail.com>
2020-11-02 22:39:04 -0800

    add the beginnings of the first function, not working as of yet

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function(){
   $("#inputNumber").submit(function(event) {
     event.preventDefault();
@@ -8,6 +6,7 @@ $(document).ready(function(){
     const replacementWords = ["Won't you be my neighbor?", "Boop!", "Beep!"];
     const numberCheck = ["3", "2", "1"];
     let range = [];
+
   function getRange(userInput) {
     range = [];
     let userNumber = parseInt(userInput);
@@ -15,24 +14,4 @@ $(document).ready(function(){
       range.push(userNumber);
       userNumber --;
     } while( UsernNumber > 0)
-    range.reverse();
-    return range;
     
-    }
-    console.log(getRange) 
-    
-  
-  
-
-
-
-    })
-      
-
-  })
-    
-
-      
-    
-  
-  
