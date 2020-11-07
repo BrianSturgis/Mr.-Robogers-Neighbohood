889f34af068c74271973e0a89bb58be52ef7aa45
Brian Sturgis <sturujisu@gmail.com>
2020-11-07 12:21:36 -0800

    update README.md and test getRange funtion

--- js/test1.js
+++ js/test1.js
@@ -1,19 +1,22 @@
-// business logic
-function getRange (answer)
+function getRange(userInput) {
+  range = [];
+} 
 
 
 
-// UI logic
+
+
+
+
 $(document).ready(function(){
   $("#inputNumber").submit(function(event){
     event.preventDefault();
     let userInput = $("#numbers").val();
-    console.log(userInput)
+    // console.log(userInput);
     const originalInput = $("#numbers").val();
+    // console.log(originalInput);
     const replacementWords = ["Won't you be my neighbor?", "Boop!", "Beep!"];
     const numberCheck = ["3", "2", "1"];
     let range = [];
-    
-    
   });
 });
