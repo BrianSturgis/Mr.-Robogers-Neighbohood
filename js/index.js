af5027e199f8f96824ad9c81260389f37e68190b
Brian Sturgis <sturujisu@gmail.com>
2020-11-08 18:51:53 -0800

    will test for branching to work

--- js/index.js
+++ js/index.js
@@ -1,30 +1,24 @@
 // business logic
-  function getNumberRange(numbers)  {
-    let numberRange = [];
-
-    for (let i = 0; i <= numbers; i++)
-      if (i.toString().includes("3")) {
-        numberRange.push("Won't you be my neighbor?");
-      } else if (i.toString().includes("2")) {
-        numberRange.push("boop");
-      } else if (i.toString().includes("1")) {
-        numberRange.push("beep");
-      } else {
-        numberRange.push(" " + i);
-      }
-      return numberRange;
+function getNumberRange(numbers)  {
+  let numberRange = [];
+  for (let i = 0; i <= numbers; i++)
+    if (i.toString().includes("3")) {
+      numberRange.push("Won't you be my neighbor?");
+    } else if (i.toString().includes("2")) {
+      numberRange.push("boop");
+    } else if (i.toString().includes("1")) {
+      numberRange.push("beep");
+    } else {
+      numberRange.push(" " + i);
     }
-    
-  
-
-  
+    return numberRange;
+  }
 //UI logic
 $(document).ready(function(){
   $("#inputNumber").submit(function(event){
     event.preventDefault();
     let numbers = parseInt($("#numbers").val());
     let answer = getNumberRange(numbers);
-
     $("p").text(answer);
     $().show();
   });
